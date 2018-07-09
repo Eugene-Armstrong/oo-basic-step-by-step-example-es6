f24db60685096814c74fee5b1190e26f5caf0526
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-09 21:43:29 +0800

    [Jeffery] Passed practice9

--- src/practice_9/class.js
+++ src/practice_9/class.js
@@ -1,1 +1,21 @@
-
+import Student from "../../src/practice_9/student.js";
+export default class Class{
+    constructor(number){
+        this.number = number;
+    }
+    leader;
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+    assignLeader(student){
+        if(this.number === student.klass.number){
+            this.leader = student;
+        }else{
+            this.leader = undefined;
+            console.log("It is not one of us.");
+        }
+    }
+    appendMember(student){
+        student.klass = this;
+    }
+}
--- src/practice_9/teacher.js
+++ src/practice_9/teacher.js
@@ -1,1 +1,22 @@
+import Person from "../../src/practice_9/person.js";
+import Student from "../../src/practice_9/student.js";
+export default class Teacher extends Person{
+    constructor(id,name,age,klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+    introduce() {
+        let cls = this.klass===undefined?`No Class.`:`${this.klass.getDisplayName()}.`;
+        return `${super.introduce()} I am a Teacher. I teach ${cls}`;
+    }
+    introduceWith(student){
+        let str = ``;
+        if(student.klass.number === this.klass.number){
+            str += `${super.introduce()} I am a Teacher. I teach ${student.name}.`
+        }else{
+            str += `${super.introduce()} I am a Teacher. I don't teach ${student.name}.`
+        }
+        return str;
+    }
+}
 
